b1032cbc501f07b72be9a317f2f3c83ac73903c4
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-19 19:45:03 +0100

    Implemented Order model

---
+++ Server/models/Order.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const orderSchema = new Schema({
+  buyerId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "buyerId is required"],
+  },
+  products: [
+    {
+      productId: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+      },
+      quantity: { type: Number, min: 1 },
+      price: {
+        type: Number,
+        required: [true, "Price is required"],
+        min: [0, "Price is required"],
+      },
+    },
+    {
+      timestamps: true,
+    },
+  ],
+});
