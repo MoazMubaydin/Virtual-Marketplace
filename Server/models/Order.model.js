195a9c47f17b0b5d112409ffeee62d2781454ddd
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-20 22:51:31 +0100

    moved timestamp from products to root

--- Server/models/Order.model.js
+++ Server/models/Order.model.js
@@ -1,29 +1,31 @@
 const { Schema, model } = require("mongoose");
 
-const orderSchema = new Schema({
-  buyerId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "buyerId is required"],
+const orderSchema = new Schema(
+  {
+    buyerId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "buyerId is required"],
+    },
+    products: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: { type: Number, min: 1 },
+        price: {
+          type: Number,
+          required: [true, "Price is required"],
+          min: [0, "Price is required"],
+        },
+      },
+    ],
   },
-  products: [
-    {
-      productId: {
-        type: Schema.Types.ObjectId,
-        ref: "Product",
-      },
-      quantity: { type: Number, min: 1 },
-      price: {
-        type: Number,
-        required: [true, "Price is required"],
-        min: [0, "Price is required"],
-      },
-    },
-    {
-      timestamps: true,
-    },
-  ],
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Order = model("Order", orderSchema);
 module.exports = Order;
