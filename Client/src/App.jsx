4e53745f1dcfbb35908f9c25ab88d45a432b38c2
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-22 12:25:20 +0100

    implemented auth and homepage routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
-
+import { Link, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signup from "./authentication/Signup";
+import Login from "./authentication/Login";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products/:productId" element={<Product />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
