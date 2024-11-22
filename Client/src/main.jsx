54e249be4db9da61125f0921177fd1302421fbb1
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-22 08:43:17 +0100

    wrapped the application with  router, authprovider and mantine

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
+import "@mantine/core/styles.css";
+import { AuthProviderWrapper } from "./context/auth.context.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <MantineProvider>
+        <AuthProviderWrapper>
+          <App />
+        </AuthProviderWrapper>
+      </MantineProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
