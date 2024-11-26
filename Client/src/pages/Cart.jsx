0073ad73c75eb8f263ebfdc5bcfa5129e11abb67
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-26 13:09:01 +0100

    implemented cart component to display items

--- Client/src/pages/Cart.jsx
+++ Client/src/pages/Cart.jsx
@@ -1,1 +1,31 @@
-export default function Cart() {}
+import { Button, Table } from "@mantine/core";
+
+export default function Cart({ cart }) {
+  const rows = cart.map((product, i) => (
+    <Table.Tr key={product._id}>
+      <Table.Td>{i}</Table.Td>
+      <Table.Td>{product.name}</Table.Td>
+      <Table.Td>{product.quantity}</Table.Td>
+      <Table.Td>{product.price}</Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <>
+      <Table.ScrollContainer minWidth={500}>
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th></Table.Th>
+              <Table.Th>Item</Table.Th>
+              <Table.Th>Quantity</Table.Th>
+              <Table.Th>Price</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+      </Table.ScrollContainer>
+      <Button>Place Order</Button>
+    </>
+  );
+}
