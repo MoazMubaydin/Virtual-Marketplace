25211c3470499f48bb5a0c4d57c019e6e6244db3
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-27 15:57:04 +0100

    added image to the table

--- Client/src/pages/cart/Cart.jsx
+++ Client/src/pages/cart/Cart.jsx
@@ -1,9 +1,12 @@
-import { Button, Table } from "@mantine/core";
+import { Button, Table, Image } from "@mantine/core";
 
 export default function Cart({ cart }) {
   const rows = cart.map((product, i) => (
     <Table.Tr key={product._id}>
       <Table.Td>{i}</Table.Td>
+      <Table.Td>
+        <Image src={product.image} height={50} radius="md" />
+      </Table.Td>
       <Table.Td>{product.name}</Table.Td>
       <Table.Td>{product.quantity}</Table.Td>
       <Table.Td>{product.price}</Table.Td>
@@ -17,6 +20,7 @@ export default function Cart({ cart }) {
           <Table.Thead>
             <Table.Tr>
               <Table.Th></Table.Th>
+              <Table.Th>Image</Table.Th>
               <Table.Th>Item</Table.Th>
               <Table.Th>Quantity</Table.Th>
               <Table.Th>Price</Table.Th>
