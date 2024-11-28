4cae609c5844483a77ca0a93a70a442e9606884a
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-28 13:12:49 +0100

    added more values to the product category

--- Client/src/components/Product/CreateProduct.jsx
+++ Client/src/components/Product/CreateProduct.jsx
@@ -1,6 +1,7 @@
 import {
   Button,
   FileInput,
+  Group,
   NumberInput,
   Select,
   Textarea,
@@ -12,22 +13,19 @@ import { AuthContext } from "../../context/auth.context";
 const DB_URL = import.meta.env.VITE_DATABASE_API_URL;
 
 export default function CreateProduct({ close, setProducts }) {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState(0);
-  const [category, setCategoroy] = useState("");
-  const [image, setImage] = useState("");
-  const [stock, setStock] = useState(1);
+  const [name, setName] = useState();
+  const [description, setDescription] = useState();
+  const [price, setPrice] = useState();
+  const [category, setCategoroy] = useState();
+  const [image, setImage] = useState();
+  const [stock, setStock] = useState();
   const [error, setError] = useState();
 
   const { user } = useContext(AuthContext);
   const imageUpload = async () => {
-    if (!image) {
-      return alert("please select an image to upload.");
-    }
     const formData = new FormData();
+    console.log(image);
     formData.append("image", image);
-
     try {
       const responce = await axios.post(`${DB_URL}/api/upload`, formData, {
         headers: { "Content-Type": "multipart/form-data" },
@@ -38,6 +36,59 @@ export default function CreateProduct({ close, setProducts }) {
       setError(error);
     }
   };
+  const categories = [
+    "Food & Beverages",
+    "Regional Delicacies",
+    "Austrian Wines",
+    "Beer & Craft Beverages",
+    "Organic & Sustainable Foods",
+    "Clothing & Traditional Wear",
+    "Dirndl & Lederhosen",
+    "Traditional Accessories",
+    "Modern Fashion",
+    "Outdoor & Alpine Clothing",
+    "Home & Living",
+    "Austrian Pottery & Ceramics",
+    "Handmade Linens & Textiles",
+    "Alpine-Style Home Décor",
+    "Furniture",
+    "Arts & Crafts",
+    "Austrian Paintings & Sculptures",
+    "Woodwork & Handcrafted Items",
+    "Traditional Folk Art",
+    "Jewelry",
+    "Books & Media",
+    "Austrian Literature & History Books",
+    "Travel Guides for Austria",
+    "Music",
+    "Films & Documentaries",
+    "Health & Beauty",
+    "Natural Skincare",
+    "Wellness & Spa Products",
+    "Austrian Health Supplements",
+    "Essential Oils",
+    "Sports & Outdoors",
+    "Ski Equipment & Gear",
+    "Hiking Accessories",
+    "Cycling Equipment",
+    "Fitness Wear",
+    "Toys & Hobbies",
+    "Traditional Wooden Toys",
+    "Board Games",
+    "Hobby Kits",
+    "Musical Instruments",
+    "Local Products",
+    "Austrian Souvenirs",
+    "Handmade Crafts from Local Artisans",
+    "Specialty Cheeses & Meats",
+    "Eco-Friendly & Sustainable Products",
+    "Seasonal Categories",
+    "Christmas Market Goods",
+    "Advent Wreaths & Calendars",
+    "Ornaments & Decorations",
+    "Glühwein Sets",
+  ];
+
   const getProducts = async () => {
     try {
       const response = await axios.get(
@@ -108,6 +159,7 @@ export default function CreateProduct({ close, setProducts }) {
       />
       <Textarea
         resize="vertical"
+        required
         label="Description"
         placeholder="Product description"
         value={description}
@@ -115,32 +167,34 @@ export default function CreateProduct({ close, setProducts }) {
           setDescription(e.target.value);
         }}
       />
-      <NumberInput
-        required
-        label="Price"
-        placeholder="Euro"
-        prefix="€"
-        mb="md"
-        value={price || 0.0}
-        onChange={(value) => {
-          setPrice(value);
-        }}
-      />
-      <NumberInput
-        label="Stock"
-        required
-        placeholder="stock"
-        mt="md"
-        value={stock || 1}
-        onChange={(value) => {
-          setStock(value);
-        }}
-      />
+      <Group>
+        <NumberInput
+          hideControls
+          required
+          label="Price"
+          placeholder="€ Euro"
+          leftSection="€"
+          value={price}
+          onChange={(value) => {
+            setPrice(value);
+          }}
+        />
+        <NumberInput
+          label="Stock"
+          hideControls
+          required
+          placeholder="stock"
+          value={stock || 1}
+          onChange={(value) => {
+            setStock(value);
+          }}
+        />
+      </Group>
       <Select
         label="Category"
         required
         placeholder="Pick value"
-        data={["food", "pottery", "jewelry, clothing", "art"]}
+        data={categories}
         value={category}
         onChange={(value) => {
           setCategoroy(value);
