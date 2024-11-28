8009e532e677873e314a2921d2dea740d0ed4a5f
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-29 00:03:31 +0100

    get order request and basic render of orders list

--- Client/src/pages/order/Order.jsx
+++ Client/src/pages/order/Order.jsx
@@ -1,16 +1,58 @@
-import { useState } from "react";
+import { Card, Table } from "@mantine/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
 const DB_URL = import.meta.env.VITE_DATABASE_API_URL;
 
 export default function Order() {
-  const [order, setOrder] = useState();
-  const [error, setError] = useState();
+  const [orders, setOrders] = useState(null);
+  const [error, setError] = useState(null);
 
-  const getOrder = async () => {
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  const getOrders = async () => {
     try {
-      const response = await axios.get(`${DB_URL}/api/orders`);
-      console.log(response);
+      const token = localStorage.getItem("authToken");
+      if (!token) {
+        console.error("No token found in localStorage");
+        return;
+      }
+      const response = await axios.get(`${DB_URL}/api/orders`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log(response.data);
+      setOrders(response.data);
     } catch (error) {
       setError(error);
     }
   };
+  if (!orders) {
+    return <p>You have no orders</p>;
+  }
+  const rows = orders.map((order, i) => {
+    return (
+      <Table.Tr key={order._id}>
+        <Table.Td>{i}</Table.Td>
+        <Table.Td>{order.createdAt}</Table.Td>
+        <Table.Td>{order.products.length}</Table.Td>
+      </Table.Tr>
+    );
+  });
+  return (
+    <>
+      <Table.ScrollContainer minWidth={500}>
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th></Table.Th>
+              <Table.Th>Ordered At</Table.Th>
+              <Table.Th>number of items</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+      </Table.ScrollContainer>
+    </>
+  );
 }
